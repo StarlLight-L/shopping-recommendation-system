869be8015e1772565b80eb791ea08529bcfeee23
StarlLight-L <xinmanli85@gmail.com>
2026-06-04 17:48:43 +1000

    Update firebase.js

--- frontend/src/api/firebase.js
+++ frontend/src/api/firebase.js
@@ -8,12 +8,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyANDTNbqDd56WpaKF_LygRzykzew_5sf7s",
-    authDomain: "imall-667d4.firebaseapp.com",
-    projectId: "imall-667d4",
-    storageBucket: "imall-667d4.firebasestorage.app",
-    messagingSenderId: "422793429515",
-    appId: "1:422793429515:web:074594d3e04291b661fd0e",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
     //measurementId: "G-V36MNREGZE"
 };
 
